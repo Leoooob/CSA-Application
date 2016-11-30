edcbddfe97469ea1e7ef0e8b0ba44fef278ed88a
Leon Hassan <leonhassan@hotmail.com>
2016-11-30 03:28:36 +0000

    Removed comment

--- app/channels/display_channel.rb
+++ app/channels/display_channel.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class DisplayChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "display_channel" # was "clock_channel"
+    stream_from "display_channel"
   end
 
   def unsubscribed
