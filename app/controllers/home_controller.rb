5a7506ff0ccb47aa23d2bbea900f01522678d491
Leon Hassan <leonhassan@hotmail.com>
2016-11-30 01:31:05 +0000

    Broadcast from home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
   skip_before_action :login_required
 
   def index
+    words = "kill me"
+    
+    ActionCable.server.broadcast 'display_channel',
+        message: '<p>' + words.to_s + '</p>'
   end
 
 end
