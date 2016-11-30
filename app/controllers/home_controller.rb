8715aa0bba02bf954023b68f09d97696da119451
Leon Hassan <leonhassan@hotmail.com>
2016-11-30 02:40:08 +0000

    Commented out test code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,12 @@ class HomeController < ApplicationController
   skip_before_action :login_required
 
   def index
-    words = "kill me"
+    =begin
+      words = "kill me"
     
-    ActionCable.server.broadcast 'display_channel',
-        message: '<p>' + words.to_s + '</p>'
+      ActionCable.server.broadcast 'display_channel',
+          message: '<p>' + words.to_s + '</p>'
+    =end
   end
 
 end
