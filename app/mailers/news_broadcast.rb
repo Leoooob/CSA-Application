64fdada962a1230da304f0b3ce69a852a4f4fe9e
Leon Hassan <leonhassan@hotmail.com>
2016-12-02 12:52:00 +0000

    Commented out SendGrid code

--- app/mailers/news_broadcast.rb
+++ app/mailers/news_broadcast.rb
@@ -1,35 +1,35 @@
-## class NewsBroadcast < ApplicationMailer
-#  def send_news(user, broadcast, email_list)
-#      @firstname = user.firstname
-#      @content = broadcast.content
-#
-#      mail to: user.email,
-#      subject: "Aber CS #{email_list} News",
-#      from: ADMIN_EMAIL
-#  end
-# end
-
-##############################################
-
 class NewsBroadcast < ApplicationMailer
-  require 'sendgrid-ruby'
-  include SendGrid
-  
   def send_news(user, broadcast, email_list)
     @firstname = user.firstname
     @content = broadcast.content
-    
-    from = ADMIN_EMAIL
-    subject = "Aber CS #{email_list}"
-    to = Email.new(email: user.email)
-    content = Content.new(type: 'text/plain', value: 'Hello, Email!')
-    
-    mail = Mail.new(from, subject, to, content)
 
-    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-    response = sg.client.mail._('send').post(request_body: mail.to_json)
-    puts response.status_code
-    puts response.body
-    puts response.headers
+    mail to: user.email,
+    subject: "Aber CS #{email_list} News",
+    from: ADMIN_EMAIL
   end
 end
+
+##############################################
+
+##class NewsBroadcast < ApplicationMailer
+#  require 'sendgrid-ruby'
+#  include SendGrid
+#  
+#  def send_news(user, broadcast, email_list)
+#    @firstname = user.firstname
+#    @content = broadcast.content
+#    
+#    from = ADMIN_EMAIL
+#    subject = "Aber CS #{email_list}"
+#    to = Email.new(email: user.email)
+#    content = Content.new(type: 'text/plain', value: 'Hello, Email!')
+#    
+#    mail = Mail.new(from, subject, to, content)
+#
+#    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+#    response = sg.client.mail._('send').post(request_body: mail.to_json)
+#    puts response.status_code
+#    puts response.body
+#    puts response.headers
+#  end
+#end
